c80e00b1bf46533ee4a7413915421600eac6d98d
Med1hani <hani.mohamed@esprit.tn>
2022-03-21 17:36:13 +0100

    ajouter les crud pour l'entite voiture

---
+++ src/Form/VoitureType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Voiture;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class VoitureType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('model')
+            ->add('marque')
+            ->add('couleur')
+            ->add('capacite')
+            ->add('description')
+            ->add('immat')
+            ->add('prix')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Voiture::class,
+        ]);
+    }
+}
